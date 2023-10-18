1832b8369bdf8ca8613b65c71a08c1847ff9c975
codersunny812 <sengersunny448@gmail.com>
2023-10-17 20:55:29 -0600

    main rendering js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './_Base.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
