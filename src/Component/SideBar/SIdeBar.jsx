068463514ef7cea8cca57806ea2c68d7ba012609
codersunny812 <sengersunny448@gmail.com>
2023-10-17 20:50:41 -0600

    side videos component

---
+++ src/Component/SideBar/SIdeBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_SIdeBar.scss'
+
+const SIdeBar = () => {
+  return (
+    <div className='border border-warning'>
+    This is the side bar component  
+    </div>
+  )
+}
+
+export default SIdeBar
