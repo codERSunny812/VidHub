6b2d81c1ba6590f73b66d14232654ec2736e42ba
codersunny812 <sengersunny448@gmail.com>
2023-10-17 20:55:55 -0600

    main js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import Header from './components/Header/Header'
-import SideBar from './components/SideBar/SideBar'
-import HomeScreen from './components/Screen/HomeScreen/HomeScreen'
+import Header from './Component/Header/Header'
+import SIdeBar from './Component/SideBar/SIdeBar'
 import {Container} from 'react-bootstrap'
+import HomeScreen from './Screen/HomeScreen/HomeScreen'
+import './_App.scss'
 function App() {
 
   return (
     <>
-      {/* header component 
-       */}
-       <Header/>
-     <div className="app_container">
-      <SideBar />
-      <Container fluid className="app_main">
-      <HomeScreen/>
-      </Container>
-     </div>
+     {/* header component */}
+      <Header/>
+      <div className="app_container border border-info">
 
+      {/* side bar component */}
+    <SIdeBar/>
+
+    <Container fluid className="app_main border border-warning">
+     <HomeScreen/>
+    </Container>
+
+
+      {/* category component */}
+
+
+    {/* videos components */}
+    </div>
     </>
   )
 }
